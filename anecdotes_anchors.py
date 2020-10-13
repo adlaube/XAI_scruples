2198430d4215bcebc9141d6b06291830c6b22a30
Adrian Lauber <lauber.adrian@gmail.com>
2020-10-13 21:41:35 +0200

    truncation for anchors

--- anecdotes_anchors.py
+++ anecdotes_anchors.py
@@ -6,7 +6,11 @@ import spacy
 instance_idx = 0
 nlp = spacy.load('en_core_web_lg')
 explainer = anchor_text.AnchorText(nlp, anecdotes_labels, use_unk_distribution=False, use_bert=True)
-exp = explainer.explain_instance(get_merged_instance(instance_idx),anecdotes_predict_anchor,threshold=0.95)
+
+
+# BERT limits to 512 tokens, anchor implementation does not take this into account 
+# so anecdotes are truncated before being explained
+exp = explainer.explain_instance(get_merged_instance(instance_idx,truncate=True),anecdotes_predict_anchor,threshold=0.95)
 exp.save_to_file(anecdotes_exp_dir+"anchor0.html")
 print('done')
 
--- anecdotes_utils.py
+++ anecdotes_utils.py
@@ -16,14 +16,30 @@ with open(anecdotes_exp_dir + 'test.scruples-corpus.jsonl', 'r') as datafile:
 anecdotes_dataset = [x['title'] + x['text'] for x in data]
 
 
-""" Argument: index to instance from dataset
-    Return:     string containing both title and body of the story """
+""" Arguments:  - index to instance from dataset
+                - truncation if needed, extract head and tail of instance
+    Return:     string containing both title and body of the story 
+    """
 
-def get_merged_instance(instance_idx):
+def get_merged_instance(instance_idx,truncate=False):
         keys = ['title', 'text']
         instance = [{x:data[instance_idx][x] for x in keys}]
-        test_features = instance[0]['title'] + instance[0]['text']
-        return test_features
+        merged_instance = instance[0]['title'] + instance[0]['text']
+
+        PARAM_MAX_WORD_LENGTH = 400
+        split_instance = merged_instance.split(" ")
+        if truncate == True:
+                total_word_count = len(split_instance)
+                if total_word_count > PARAM_MAX_WORD_LENGTH: #poor estimate from word count to tokens, tokens are limited to 512
+                        excess_word_count = total_word_count - PARAM_MAX_WORD_LENGTH
+                        split_idx_head = np.uint32((total_word_count - excess_word_count) / 2)
+                        split_idx_tail = np.uint32((total_word_count + excess_word_count) / 2)
+                        truncated_instance = split_instance[:split_idx_head] + split_instance[split_idx_tail:]
+                        #join back to string
+                        truncated_instance = ' '.join(truncated_instance)
+                        return truncated_instance
+
+        return merged_instance
 
 def anecdotes_predict_lime(texts):
 
@@ -60,8 +76,8 @@ def anecdotes_predict_anchor(texts):
                 }
                 instances.append(instance)
         
-        #response = requests.post('https://norms.apps.allenai.org/api/corpus/predict',json=instances)
-        response = requests.post('http://127.0.0.1:5050/api/corpus/predict',json=instances)
+        response = requests.post('https://norms.apps.allenai.org/api/corpus/predict',json=instances)
+        #response = requests.post('http://127.0.0.1:5050/api/corpus/predict',json=instances)
         response_json = json.loads(response.text) #will throw exception if num of samples is too high
         #prediction is the label with the highest alpha score
         predicted_labels = [max(response_dict, key=response_dict.get)
