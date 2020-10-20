d73444cae0ad6adfbcef9a8ed1fcce5d5405945c
adlaube <adlaube@stern>
2020-10-20 22:47:49 +0200

    shap

--- anecdotes_shap.py
+++ anecdotes_shap.py
@@ -1,21 +1,31 @@
-from anecdotes_utils import anecdotes_predict_lime, anecdotes_labels, get_merged_instance, anecdotes_exp_dir, anecdotes_dataset
+from anecdotes_utils import anecdotes_labels, get_merged_instance, anecdotes_exp_dir, anecdotes_dataset
 import shap
 import numpy as np
 from scruples.demos.norms.app import load_model
+import torch
 
-anecdotes_dataset_array = np.array(anecdotes_dataset)
 model,featurize,delabelize = load_model(dataset='corpus')
 
 
-instance_idx = 0
-test_sample = get_merged_instance(instance_idx,truncate=True)
+# dataset = utils.PredictionDataset(
+#         features=anecdotes_dataset,
+#         transform=featurize)
+# data_loader = torch.utils.data.DataLoader(
+#         dataset=dataset,
+#         batch_size=settings.NORMS_PREDICT_BATCH_SIZE,
+#         shuffle=False)
 
-#background dataset that can be used as expectation
-background = anecdotes_dataset_array[np.random.choice(len(anecdotes_dataset),100,replace=False)]
-device = get_device()
-with torch.no_grad():
-        background.to(device)
-explainershap = shap.DeepExplainer(model,background)
+anecdotes_array = np.array(anecdotes_dataset)
+anecdotes_array = anecdotes_array[np.random.choice(len(anecdotes_dataset),100,replace=False)]#cast to array to perform array indexing
+anecdotes_list = anecdotes_array.tolist()
+
+
+featured_list = []
+for idx, anecdote in enumerate(anecdotes_list):
+        featured_list.append(featurize(anecdote))
+
+test_sample = featurize(anecdotes_dataset[0])
+explainershap = shap.DeepExplainer(model,featured_list)
 shap_values_matrix = explainershap.shap_values(test_sample)
 shap.summary_plot(shap_values_matrix,test_sample)#ä,feature_names=dataset.feature_names)
 print('done')
---
+++ start_anchor.sh
@@ -0,0 +1,5 @@
+rm log/anchor.log
+
+setsid python anecdotes_anchors.py > log/anchor.log &
+
+
