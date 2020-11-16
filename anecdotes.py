9533bf5c784e9b3412e8372c80d3aa6244aca688
adlaube <adlaube@stern>
2020-11-16 21:11:09 +0100

    output html with soup

--- anecdotes.py
+++ anecdotes.py
@@ -4,6 +4,8 @@ from anecdotes_utils import anecdotes_data,anecdotes_labels
 import numpy as np
 import pandas as pd
 
+from bs4 import BeautifulSoup
+
 if __name__ == "__main__":
 
         #np.random.randint(0,2500,50,np.uint32)
@@ -12,6 +14,8 @@ if __name__ == "__main__":
                                          434,  946,  220, 1317,  877, 2077, 1559, 1509,  439,  670, 2470,
                                          203, 1949,  138, 2437,  506,  401, 2212, 1652,  807,  221,  110,
                                  2478,  567,  551,   69, 1911, 1851],dtype=np.uint32)
+
+        sample_indices = np.array([1,2,3])
         
         anecdotes_df = pd.DataFrame(anecdotes_data)
         anecdotes_df = anecdotes_df.iloc[sample_indices]
@@ -27,10 +31,22 @@ if __name__ == "__main__":
                         }
 
 
-
+        out_soup = None
         for idx in sample_indices:
                 exp = explain_anecdote_lime(idx,param_dict)
-                
+                exp_html = exp.as_html()
+                exp_soup = BeautifulSoup(exp_html)
+
+                if out_soup is None:
+                        out_soup = exp_soup
+                else:
+                        out_soup.body.append(exp_soup.body)
+                print('done')
+
+        
+        with open("test.html","w") as file:
+                file.write(str(out_soup))
+
                 #explain_anecdote_lime(idx,10,10000)
         
     
