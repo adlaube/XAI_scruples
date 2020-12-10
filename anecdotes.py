ec1c8f7e34abb97652a9631b667f5a2c744bc15a
adlaube <adlaube@stern>
2020-12-10 19:53:33 +0100

    fix if not executed in git directory

--- anecdotes.py
+++ anecdotes.py
@@ -82,7 +82,10 @@ if __name__ == "__main__":
 	meta_df["Date"] = meta_df["Date"].astype(str)
 	meta_df["Date"] = [datetime.now().strftime("%d/%m/%Y %H:%M:%S")]
 	meta_df["Code commit"] = meta_df["Code commit"].astype(str)
-	meta_df["Code commit"] = subprocess.check_output(["git", "describe"]).strip()
+	try:
+		meta_df["Code commit"] = subprocess.check_output(["git", "describe"]).strip()
+	except:
+		pass
 	meta_df["Sample selection"] = meta_df["Sample selection"].astype(str)
 	meta_df["Sample selection"] = "random"
 	meta_df.transpose()
